622fa4c275fb51ad14f21a4bc669989621753775
Sherif <sherif.rafik@espace.com.eg>
2023-12-09 00:03:58 +0000

    Add tokenable concern

---
+++ app/models/concerns/tokenable.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module Tokenable
+  extend ActiveSupport::Concern
+  
+  included do
+    before_validation :generate_token
+  end
+
+  private 
+
+  def generate_token
+    token = SecureRandom.hex(16)
+
+    while InMemoryDataStore.exists?(token)
+      token = SecureRandom.hex(16)
+    end
+
+    self.token = token
+  end
+end
