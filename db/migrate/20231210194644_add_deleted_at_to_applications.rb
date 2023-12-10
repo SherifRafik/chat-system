a27f9ec6bc39fb6dc124158189cf79ea757d6ef9
Sherif <sherif.rafik@espace.com.eg>
2023-12-10 20:07:54 +0000

    Delete deleted_at migration

--- db/migrate/20231210194644_add_deleted_at_to_applications.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class AddDeletedAtToApplications < ActiveRecord::Migration[7.1]
-  def change
-    change_table :applications, bulk: true do |t|
-      t.datetime :deleted_at, index: true
-    end
-  end
-end
