6885decd9d657a87b1b01ad224266b61a6fb2b10
Sherif <sherif.rafik@espace.com.eg>
2023-12-10 20:34:58 +0000

    Add chat creator job

---
+++ app/sidekiq/chat_creator_job.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class ChatCreatorJob
+  include Sidekiq::Job
+
+  def perform(application_token, number)
+    ActiveRecord::Base.transaction do
+      application = Application.lock.find_by(token: application_token)
+
+      if application.present?
+        Chat.create(application_token: application_token, number: number)
+        updated_chats_count = application.chats_count + 1
+        application.update(chats_count: updated_chats_count)
+      end
+    end
+  end
+end
