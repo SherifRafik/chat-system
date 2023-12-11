e7a96176284f8c9f95d0b38b62b07574cd835262
Sherif <sherif.rafik@espace.com.eg>
2023-12-11 16:28:49 +0000

    Fix chat creator error

--- app/sidekiq/chat_creator_job.rb
+++ app/sidekiq/chat_creator_job.rb
@@ -8,7 +8,7 @@ class ChatCreatorJob
       application = Application.lock.find_by(token: application_token)
 
       if application.present?
-        Chat.create(application_token: application_token, number: number)
+        Chat.create(application: application, number: number)
         updated_chats_count = application.chats_count + 1
         application.update(chats_count: updated_chats_count)
       end
