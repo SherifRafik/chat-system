d82b1e029214f5fe5dcb9f25013de4c21c31b908
Sherif <sherif.rafik@espace.com.eg>
2023-12-11 16:24:15 +0000

    Refactor application destroyer

--- app/sidekiq/application_destroyer_job.rb
+++ app/sidekiq/application_destroyer_job.rb
@@ -5,21 +5,30 @@ class ApplicationDestroyerJob
 
   def perform(application_token)
     ActiveRecord::Base.transaction do
-      application = Application.find_by(token: application_token)
+      @application = Application.find_by(token: application_token)
       if application.present?
-        application_token = application.token
-        # Delete the token from redis (To prevent the chat creator job from queueinq)
-        InMemoryDataStore.hdel(APPLICATION_HASH_KEY, application_token)
-        # Delete the chats from redis (to prevent the message creator job from queuing)
-        chats = application.chats
-
-        chats.each do |chat|
-          generated_chat_key = KeyGenerator.generate_chat_key(application_token: application_token, number: chat.number)
-          InMemoryDataStore.hdel(CHAT_HASH_KEY, generated_chat_key)
-        end
-        # Destroy the application
+        delete_application_from_memory_datastore(application)
+        delete_chats_from_memory_datastore(application)
         application.destroy
       end
+    end
+  end
+
+  private
+
+  attr_reader :application
+
+  def delete_application_from_memory_datastore
+    # Delete the token from redis (To prevent the chat creator job from queueinq)
+    InMemoryDataStore.hdel(APPLICATION_HASH_KEY, application.token)
+  end
+
+  def delete_chats_from_memory_datastore
+    # Delete the chats from redis (to prevent the message creator job from queuing)
+    chats = application.chats
+    chats.each do |chat|
+      generated_chat_key = KeyGenerator.generate_chat_key(application_token: application.token, number: chat.number)
+      InMemoryDataStore.hdel(CHAT_HASH_KEY, generated_chat_key)
     end
   end
 end
