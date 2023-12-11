102cb4faf4407c27206ff7604da2f23d347c33c6
Sherif <sherif.rafik@espace.com.eg>
2023-12-11 14:08:08 +0000

    Mount sidekiq route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
+require 'sidekiq/web'
+
+Sidekiq::Web.use ActionDispatch::Cookies
+Sidekiq::Web.use ActionDispatch::Session::CookieStore, key: "_interslice_session"
+
 Rails.application.routes.draw do
+  mount Sidekiq::Web => '/sidekiq'
+
   get 'up' => 'rails/health#show', as: :rails_health_check
 
   namespace :api do
