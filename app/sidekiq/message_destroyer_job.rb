bfe4f7205b6529c6ceb5ebd53f429027dfe3c365
Sherif <sherif.rafik@espace.com.eg>
2023-12-12 16:15:48 +0000

    Add message destroyer job

---
+++ app/sidekiq/message_destroyer_job.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+class MessageDestroyerJob
+  include Sidekiq::Job
+
+  def perform(application_token, chat_number, number)
+    @application_token = application_token
+    @chat_number = chat_number
+
+    ActiveRecord::Base.transaction do
+      application = Application.find_by(token: application_token)
+      if application.present?
+        chat = application.chats.find_by(number: chat_number)
+        if chat.present?
+          message = chat.messages.find_by(number: number)
+          if message.present?
+            message.destroy
+          elsif message_count_in_memory.present? && message_count_in_memory.to_i >= number
+            MessageDestroyerJob.perform_in(30.seconds, application_token, chat_number, number)
+          end
+        end
+      end
+    end
+  end
+
+  private
+
+  attr_reader :application_token, :chat_number
+
+  def message_count_in_memory
+    InMemoryDataStore.hget(CHAT_HASH_KEY, generate_chat_key)
+  end
+
+  def generate_chat_key
+    KeyGenerator.generate_chat_key(application_token: application_token, number: chat_number)
+  end
+end
