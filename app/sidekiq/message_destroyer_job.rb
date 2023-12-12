ef4895764a633bd6c1275536c747a9e8246140ee
Sherif <sherif.rafik@espace.com.eg>
2023-12-12 21:26:13 +0200

    Decrement the message count in memory and in db after message destruction

--- app/sidekiq/message_destroyer_job.rb
+++ app/sidekiq/message_destroyer_job.rb
@@ -10,11 +10,13 @@ class MessageDestroyerJob
     ActiveRecord::Base.transaction do
       application = Application.find_by(token: application_token)
       if application.present?
-        chat = application.chats.find_by(number: chat_number)
+        @chat = Chat.lock.find_by(application_id: application.id, number: chat_number)
         if chat.present?
           message = chat.messages.find_by(number: number)
           if message.present?
             message.destroy
+            decrement_messages_count_in_chat
+            decrement_messages_count_in_memory
           elsif chat_exists_in_memory? && message_count_in_memory.to_i >= number
             MessageDestroyerJob.perform_in(30.seconds, application_token, chat_number, number)
           end
@@ -27,7 +29,7 @@ class MessageDestroyerJob
 
   private
 
-  attr_reader :application_token, :chat_number
+  attr_reader :application_token, :chat_number, :chat
 
   def message_count_in_memory
     InMemoryDataStore.hget(CHAT_HASH_KEY, generate_chat_key)
@@ -39,5 +41,14 @@ class MessageDestroyerJob
 
   def generate_chat_key
     KeyGenerator.generate_chat_key(application_token: application_token, number: chat_number)
+  end
+
+  def decrement_messages_count_in_chat
+    updated_messages_count = chat.messages_count - 1
+    chat.update(messages_count: updated_messages_count)
+  end
+
+  def decrement_messages_count_in_memory
+    InMemoryDataStore.hincrby(CHAT_HASH_KEY, generate_chat_key, -1)
   end
 end
