2c8df9c0b9cec2f931e58c8a0654e205e9571227
Sherif <sherif.rafik@espace.com.eg>
2023-12-12 16:15:41 +0000

    Add message destroyer service

---
+++ app/services/messages/message_destroyer.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+module Messages
+  class MessageDestroyer
+    def initialize(application_token:, chat_number:, number:)
+      @application_token = application_token
+      @chat_number = chat_number
+      @number = number
+    end
+
+    def call
+      if message_count_in_memory.present? && message_count_in_memory.to_i >= number
+        MessageDestroyerJob.perform_async(application_token, chat_number, number)
+      else
+        false
+      end
+    end
+
+    private
+
+    attr_reader :application_token, :chat_number, :number
+
+    def message_count_in_memory
+      InMemoryDataStore.hget(CHAT_HASH_KEY, generate_chat_key)
+    end
+
+    def generate_chat_key
+      KeyGenerator.generate_chat_key(application_token: application_token, number: chat_number)
+    end
+  end
+end
