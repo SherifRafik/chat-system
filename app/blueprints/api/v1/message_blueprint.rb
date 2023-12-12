2ec34a040d9f7d2c9ab4148b832d3d4b10b8dc7a
Sherif <sherif.rafik@espace.com.eg>
2023-12-12 20:13:49 +0200

    Remove application controller from message blueprint

--- app/blueprints/api/v1/message_blueprint.rb
+++ app/blueprints/api/v1/message_blueprint.rb
@@ -4,7 +4,7 @@ module Api
   module V1
     class MessageBlueprint < Blueprinter::Base
       identifier :number
-      fields :chat_number, :application_token, :body, :created_at, :updated_at
+      fields :chat_number, :body, :created_at, :updated_at
     end
   end
 end
