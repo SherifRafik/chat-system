c52bc7e7f15a081bc8bbddccb7564e65ee1658c8
Sherif <sherif.rafik@espace.com.eg>
2023-12-12 16:34:06 +0000

    Remove the unnecessary method from chat destroyer (No need to store messages in datastore, rely on messages count)

--- app/sidekiq/chat_destroyer_job.rb
+++ app/sidekiq/chat_destroyer_job.rb
@@ -10,7 +10,6 @@ class ChatDestroyerJob
         @chat = application.chats.find_by(number: number)
         if chat.present?
           delete_chat_from_memory_datastore(application_token, number)
-          delete_messages_from_memory_datastore
           chat.destroy
         elsif chat_exists_in_memory?(application_token, number)
           ChatDestroyerJob.perform_in(30.seconds, application_token, number)
@@ -29,10 +28,6 @@ class ChatDestroyerJob
 
   def generate_chat_key(application_token, number)
     KeyGenerator.generate_chat_key(application_token: application_token, number: number)
-  end
-
-  def delete_messages_from_memory_datastore
-    # TODO: Implement this when adding the messages
   end
 
   def chat_exists_in_memory?(application_token, number)
