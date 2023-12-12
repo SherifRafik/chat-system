0e612c0f317d7d90a340036647775aebe05e66fa
Sherif <sherif.rafik@espace.com.eg>
2023-12-12 16:15:33 +0000

    Check that application is present in chat creator job

--- app/sidekiq/message_creator_job.rb
+++ app/sidekiq/message_creator_job.rb
@@ -6,14 +6,16 @@ class MessageCreatorJob
   def perform(application_token, chat_number, number, body)
     ActiveRecord::Base.transaction do
       application = Application.find_by(token: application_token)
-      chat = Chat.lock.find_by(application_id: application.id, number: chat_number)
+      if application.present?
+        chat = Chat.lock.find_by(application_id: application.id, number: chat_number)
 
-      if chat.present?
-        Message.create(body: body, chat: chat, number: number)
-        updated_messages_count = chat.messages_count + 1
-        chat.update(messages_count: updated_messages_count)
-      elsif chat_exists_in_memory?(application_token, chat_number)
-        MessageCreatorJob.perform_in(30.seconds, application_token, chat_number, number, body)
+        if chat.present?
+          Message.create(body: body, chat: chat, number: number)
+          updated_messages_count = chat.messages_count + 1
+          chat.update(messages_count: updated_messages_count)
+        elsif chat_exists_in_memory?(application_token, chat_number)
+          MessageCreatorJob.perform_in(30.seconds, application_token, chat_number, number, body)
+        end
       end
     end
   end
